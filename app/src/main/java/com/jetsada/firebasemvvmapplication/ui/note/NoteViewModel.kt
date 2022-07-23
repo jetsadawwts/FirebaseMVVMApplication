3da8e699e7243354d9ecbd786d8354ce0a930b06
Jetsadawwts <jetsadawwts@gmail.com>
2022-07-23 13:06:07 +0700

    Firebase delete data and new ui

--- app/src/main/java/com/jetsada/firebasemvvmapplication/ui/note/NoteViewModel.kt
+++ app/src/main/java/com/jetsada/firebasemvvmapplication/ui/note/NoteViewModel.kt
@@ -3,48 +3,79 @@ package com.jetsada.firebasemvvmapplication.ui.note
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.jetsada.firebasemvvmapplication.data.repository.NoteRepository
 import com.jetsada.firebasemvvmapplication.model.Note
 import com.jetsada.firebasemvvmapplication.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteViewModel @Inject constructor(private val repository: NoteRepository): ViewModel() {
 
-    private val _note = MutableLiveData <UiState<List<Note>>>()
-    val note: LiveData<UiState<List<Note>>> get() = _note
+//    private val _note = MutableLiveData<ViewState>()
+//    val note: LiveData<ViewState> get() = _note
 
-    private val _addNote = MutableLiveData<UiState<String>>()
-    val addNote: LiveData<UiState<String>> get() = _addNote
+    private val _notes = MutableLiveData<UiState<List<Note>>>()
+    val note: LiveData<UiState<List<Note>>>
+        get() = _notes
+
+    private val _addNote = MutableLiveData<UiState<Pair<Note,String>>>()
+    val addNote: LiveData<UiState<Pair<Note,String>>>
+        get() = _addNote
 
     private val _updateNote = MutableLiveData<UiState<String>>()
-    val updateNote: LiveData<UiState<String>> get() = _updateNote
+    val updateNote: LiveData<UiState<String>>
+        get() = _updateNote
 
-    fun addNote(note: Note) {
-        _addNote.value = UiState.Loading
-            repository.addNotes(note) {
-                _addNote.value = it
-            }
-    }
-
-    fun updateNote(note: Note) {
-        _updateNote.value = UiState.Loading
-        repository.updateNotes(note) {
-            _updateNote.value = it
-        }
-    }
+    private val _deleteNote = MutableLiveData<UiState<String>>()
+    val deleteNote: LiveData<UiState<String>>
+        get() = _deleteNote
 
     fun getNotes() {
-        _note.value = UiState.Loading
-            repository.getNotes {
-                _note.value = it
-            }
+        _notes.value = UiState.Loading
+        repository.getNotes { _notes.value = it }
     }
+
+    fun addNote(note: Note){
+        _addNote.value = UiState.Loading
+        repository.addNote(note) { _addNote.value = it }
+    }
+
+    fun updateNote(note: Note){
+        _updateNote.value = UiState.Loading
+        repository.updateNote(note) { _updateNote.value = it }
+    }
+
+    fun deleteNote(note: Note){
+        _deleteNote.value = UiState.Loading
+        repository.deleteNote(note) { _deleteNote.value = it }
+    }
+
+//    fun getNotes() {
+//            _note.value = ViewState(isLoading = true)
+//            repository.getNotes {
+//                when (it) {
+//
+//                    is UiState.Loading -> {
+//                        _note.value = ViewState(isLoading = true)
+//                    }
+//
+//                    is UiState.Success -> {
+//                        _note.value = ViewState(NoteList = it.data)
+//                    }
+//                    is UiState.Failure -> {
+//                        _note.value = ViewState(error = it.error.toString() ?: "An Unexpected Error")
+//                    }
+//                }
+//            }
+//    }
+//
+//
+//    data class ViewState (
+//        val isLoading : Boolean = false,
+//        val NoteList : List<Note> = emptyList(),
+//        val error : String = ""
+//   )
 
 //    fun getNotes() {
 //        _note.value = UiState.Loading
